e0ea92747e75990710c6294d9e6e38926b628583
Couragyn Chretien <couragyn@gmail.com>
2017-03-13 19:37:48 +0000

    step 2

--- printHtml.js
+++ printHtml.js
@@ -1,28 +1,27 @@
 var https = require('https');
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
+
+  var output = '';
 
   https.get(requestOptions, function (response) {
 
     response.setEncoding('utf8');
 
     response.on('data', function (data) {
-      console.log(data.toString());
-      console.log('\n');
-      console.log('================================');
-
+      output += data + '\n==============';
     });
 
     response.on('end', function() {
-      console.log("response stream comlete");
+      console.log(output);
     });
   });
 
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
